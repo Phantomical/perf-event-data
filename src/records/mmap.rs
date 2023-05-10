cecd9cf801bfa08964daf7acd1278136a0b00adb
Sean Lynch <phantom@lynches.ca>
2023-05-09 23:58:04 -0700

    Document Mmap

--- src/records/mmap.rs
+++ src/records/mmap.rs
@@ -1,4 +1,5 @@
 use std::borrow::Cow;
+use std::ffi::OsStr;
 
 use crate::prelude::*;
 use crate::Mmap2;
@@ -11,22 +12,58 @@ use crate::Mmap2;
 /// [manpage]: http://man7.org/linux/man-pages/man2/perf_event_open.2.html
 #[derive(Clone, Debug)]
 pub struct Mmap<'a> {
+    /// The process ID.
     pub pid: u32,
+
+    /// The thread ID.
     pub tid: u32,
+
+    /// The address that the mapping was placed at in the process' address
+    /// space.
     pub addr: u64,
+
+    /// The length, in bytes, of the allocated memory.
     pub len: u64,
+
+    /// The page offset of the memory mapping.
     pub pgoff: u64,
+
+    /// The path to the file that is being mapped, if there is one.
+    ///
+    /// # Notes
+    /// - Not all memory mappings have a path on the file system. In cases where
+    ///   there is no such path then this will be a label(ish) string from the
+    ///   kernel (e.g. `[stack]`, `[heap]`, `[vdso]`, etc.)
+    /// - Just because the mapping has a path doesn't necessarily mean that the
+    ///   file at that path was the file that was mapped. The file may have been
+    ///   deleted in the meantime or the process may be under a chroot.
+    ///
+    /// If you need to be able to tell whether the file at the path is the same
+    /// one as was mapped you will need to use [`Mmap2`] instead.
     pub filename: Cow<'a, [u8]>,
 }
 
 impl<'a> Mmap<'a> {
+    /// The path to the file that is being mapped, as an [`OsStr`].
+    ///
+    /// # Notes
+    /// - Not all memory mappings have a path on the file system. In cases where
+    ///   there is no such path then this will be a label(ish) string from the
+    ///   kernel (e.g. `[stack]`, `[heap]`, `[vdso]`, etc.)
+    /// - Just because the mapping has a path doesn't necessarily mean that the
+    ///   file at that path was the file that was mapped. The file may have been
+    ///   deleted in the meantime or the process may be under a chroot.
+    ///
+    /// If you need to be able to tell whether the file at the path is the same
+    /// one as was mapped you will need to use [`Mmap2`] instead.
     #[cfg(unix)]
-    pub fn filename_os(&self) -> &std::ffi::OsStr {
+    pub fn filename_os(&self) -> &OsStr {
         use std::os::unix::ffi::OsStrExt;
 
         OsStrExt::from_bytes(&self.filename)
     }
 
+    /// Convert all the borrowed data in this `CGroup` into owned data.
     pub fn into_owned(self) -> Mmap<'static> {
         Mmap {
             filename: self.filename.into_owned().into(),

1141e911d1a7cdb8d8a203e5250d30f33d8d9675
Sean Lynch <phantom@lynches.ca>
2023-05-13 15:22:37 -0700

    Expand the top-level documentation with an example

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,32 @@
-//! Rust structs for records emitted by `perf` and `perf_event_open`.
+//! Parse data emitted by the linux `perf_event_open` syscall.
+//!
+//! To parse data you will need the types in the [`parse`] module.
+//!
+//! # Example
+//! ```
+//! # fn main() -> perf_event_data::parse::Result<()> {
+//! use perf_event_data::parse::{ParseConfig, Parser};
+//! use perf_event_data::endian::Native;
+//! use perf_event_data::Record;
+//!
+//! let data: &[u8] = &[
+//!     1, 0, 0, 0,
+//!     40, 0,
+//!     0, 0,
+//!     22, 76, 1, 0, 23, 76, 1, 0, 0, 160, 72, 150, 79, 127, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0,
+//!     160, 72, 150, 79, 127, 0, 0, 47, 47, 97, 110, 111, 110, 0, 0,
+//! ];
+//! let config = ParseConfig::<Native>::default();
+//! let mut parser = Parser::new(data, config);
+//! let record: Record = parser.parse()?;
+//! 
+//! let mmap = match record {
+//!     Record::Mmap(mmap) => mmap,
+//!     _ => panic!("expected a MMAP record")
+//! };
+//! # Ok(())
+//! # }
+//! ```
 
 #![warn(missing_docs)]
 

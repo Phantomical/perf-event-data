cb824fd36be83a06fd8cc301dc3e4582e035ccf1
Sean Lynch <phantom@lynches.ca>
2023-05-13 15:53:36 -0700

    Rename parse::Result -> ParseResult

--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,21 @@
 //! # Example
 //! ```
 //! # fn main() -> perf_event_data::parse::Result<()> {
-//! use perf_event_data::parse::{ParseConfig, Parser};
 //! use perf_event_data::endian::Native;
+//! use perf_event_data::parse::{ParseConfig, Parser};
 //! use perf_event_data::Record;
 //!
 //! let data: &[u8] = &[
-//!     1, 0, 0, 0,
-//!     40, 0,
-//!     0, 0,
-//!     22, 76, 1, 0, 23, 76, 1, 0, 0, 160, 72, 150, 79, 127, 0, 0, 0, 16, 0, 0, 0, 0, 0, 0, 0,
-//!     160, 72, 150, 79, 127, 0, 0, 47, 47, 97, 110, 111, 110, 0, 0,
+//!     1, 0, 0, 0, 40, 0, 0, 0, 22, 76, 1, 0, 23, 76, 1, 0, 0, 160, 72, 150, 79, 127, 0, 0, 0, 16,
+//!     0, 0, 0, 0, 0, 0, 0, 160, 72, 150, 79, 127, 0, 0, 47, 47, 97, 110, 111, 110, 0, 0,
 //! ];
 //! let config = ParseConfig::<Native>::default();
 //! let mut parser = Parser::new(data, config);
 //! let record: Record = parser.parse()?;
-//! 
+//!
 //! let mmap = match record {
 //!     Record::Mmap(mmap) => mmap,
-//!     _ => panic!("expected a MMAP record")
+//!     _ => panic!("expected a MMAP record"),
 //! };
 //! # Ok(())
 //! # }

8b711f634c09cf38623cd2b6951ffa1152db14c1
Sean Lynch <phantom@lynches.ca>
2023-05-14 05:33:15 -0400

    Write README and more library docs (#16)

---
+++ README.md
@@ -0,0 +1,57 @@
+# perf-event-data
+
+<div style="text-align: center">
+
+![ci badge]
+[![crates.io badge]](https://crates.io/crates/perf-event-data)
+[![docs.rs badge]](https://docs.rs/perf-event-data)
+
+</div>
+
+[ci badge]: https://img.shields.io/github/actions/workflow/status/phantomical/perf-event-data/dispatch.yml?branch=master&style=flat-square
+[docs.rs badge]: https://img.shields.io/docsrs/perf-event-data?style=flat-square
+[crates.io badge]: https://img.shields.io/crates/v/perf-event-data?style=flat-square
+
+Parse data emitted by [`perf_event_open`] into usable rust structs.
+
+[`perf_event_open`]: https://man7.org/linux/man-pages/man2/perf_event_open.2.html
+
+## Getting Started
+
+- The `Record` type is an enum with every known record type.
+- The `parse` module has what you need to parse bytes into known records.
+
+Putting it all together, we get
+
+```rust
+use perf_event_data::endian::Native;
+use perf_event_data::parse::{ParseConfig, Parser};
+use perf_event_data::Record;
+
+fn main() {
+    let data: &[u8] = &[
+        0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x30, 0x00,
+        0x16, 0x4C, 0x01, 0x00, 0x17, 0x4C, 0x01, 0x00,
+        0x00, 0xA0, 0x48, 0x96, 0x4F, 0x7F, 0x00, 0x00,
+        0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0xA0, 0x48, 0x96, 0x4F, 0x7F, 0x00, 0x00,
+        0x2F, 0x2F, 0x61, 0x6E, 0x6F, 0x6E, 0x00, 0x00, 
+    ];
+
+    let config = ParseConfig::<Native>::default();
+    let mut parser = Parser::new(data, config);
+    let record: Record = parser.parse().expect("failed to parse the record");
+
+    // ...
+}
+```
+
+# See Also
+- The [`perf-event2`] crate allows you to set up sampled perf event counters.
+  You will likely need this, or an equivalent crate, in order to get the event
+  data this crate parses.
+- The [`linux-perf-event-reader`] crate provides similar functionality to this
+  one.
+
+[`perf-event2`]: https://crates.io/crates/perf-event2
+[`linux-perf-event-reader`]: https://crates.io/crates/linux-perf-event-reader
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,12 @@
 //! Parse data emitted by the linux `perf_event_open` syscall.
 //!
-//! This crate is organized like this:
-//! - The root contains all the data types that records can be parsed into. This
-//!   includes not only the types corresponding to the perf records but also
-//!   those that make up their fields, and so on.
-//! - The [`parse`][mod] module contains types and traits needed to implement
-//!   parsing support. Most types exposed in the root implement [`Parse`] but to
-//!   actually make use of that you will need the [`Parser`] and [`ParseConfig`]
-//!   types from the [`parse`][mod] module.
-//! - The [`endian`] module contains some types for converting values to the
-//!   native endian. You likely won't have to interact with it other than
-//!   picking one type to use when creating a [`ParseConfig`].
+//! # Important Types
+//! - [`Record`] is an enum that can parse any known record type emitted by
+//!   [`perf_event_open`]. You will likely want to start here.
+//! - The [`parse`] module has the [`Parser`] and [`ParseConfig`] types which
+//!   you will need in order to parse anything.
+//!
+//! [`perf_event_open`]: https://man7.org/linux/man-pages/man2/perf_event_open.2.html
 //!
 //! [mod]: crate::parse
 //! [`Parse`]: crate::parse::Parse
@@ -26,9 +22,12 @@
 //! use perf_event_data::Record;
 //!
 //! let data: &[u8] = &[
-//!     1, 0, 0, 0, 0, 0, 48, 0, 22, 76, 1, 0, 23, 76, 1, 0, 0, 160, 72, 150, 79, 127, 0, 0, 0, 16,
-//!     0, 0, 0, 0, 0, 0, 0, 160, 72, 150, 79, 127, 0, 0, 47, 47, 97, 110, 111, 110, 0, 0,
+//!     0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x30, 0x00, 0x16, 0x4C, 0x01, 0x00, 0x17, 0x4C, 0x01,
+//!     0x00, 0x00, 0xA0, 0x48, 0x96, 0x4F, 0x7F, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00,
+//!     0x00, 0x00, 0x00, 0xA0, 0x48, 0x96, 0x4F, 0x7F, 0x00, 0x00, 0x2F, 0x2F, 0x61, 0x6E, 0x6F,
+//!     0x6E, 0x00, 0x00,
 //! ];
+//!
 //! let config = ParseConfig::<Little>::default();
 //! let mut parser = Parser::new(data, config);
 //! let record: Record = parser.parse()?;
@@ -40,6 +39,19 @@
 //! # Ok(())
 //! # }
 //! ```
+//!
+//! # Crate Orginization
+//! This crate is organized like this:
+//! - The root contains all the data types that records can be parsed into. This
+//!   includes not only the types corresponding to the perf records but also
+//!   those that make up their fields, and so on.
+//! - The [`parse`][mod] module contains types and traits needed to implement
+//!   parsing support. Most types exposed in the root implement [`Parse`] but to
+//!   actually make use of that you will need the [`Parser`] and [`ParseConfig`]
+//!   types from the [`parse`][mod] module.
+//! - The [`endian`] module contains some types for converting values to the
+//!   native endian. You likely won't have to interact with it other than
+//!   picking one type to use when creating a [`ParseConfig`].
 //!
 //! # Parsing `perf.data` files
 //! This crate doesn't yet have support for this, although it could be used as
@@ -79,15 +91,27 @@ pub use crate::visitor::{RecordMetadata, Visitor};
 ///
 /// This way it doesn't need to be repeated multiple times unless we want to
 /// show it as part of the doc test.
+///
+/// It is also used to verify that the examples within the README work.
 #[doc(hidden)]
 pub mod doctest {
+    #[doc = include_str!("../README.md")]
+    pub mod readme {}
+
     pub const MMAP: &[u8] = &[
-        1, 0, 0, 0, 0, 0, 48, 0, 22, 76, 1, 0, 23, 76, 1, 0, 0, 160, 72, 150, 79, 127, 0, 0, 0, 16,
-        0, 0, 0, 0, 0, 0, 0, 160, 72, 150, 79, 127, 0, 0, 47, 47, 97, 110, 111, 110, 0, 0,
+        0x01, 0x00, 0x00, 0x00, // type (MMAP)
+        0x00, 0x00, // misc
+        0x30, 0x00, // size
+        0x16, 0x4C, 0x01, 0x00, // pid
+        0x17, 0x4C, 0x01, 0x00, // tid
+        0x00, 0xA0, 0x48, 0x96, 0x4F, 0x7F, 0x00, 0x00, // addr
+        0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // len
+        0x00, 0xA0, 0x48, 0x96, 0x4F, 0x7F, 0x00, 0x00, // pgoff
+        0x2F, 0x2F, 0x61, 0x6E, 0x6F, 0x6E, 0x00, 0x00, // filename
     ];
 
     pub const CUSTOM_SAMPLE: &[u8] = &[
-        0x09, 0x00, 0x00, 0x00, // type
+        0x09, 0x00, 0x00, 0x00, // type (SAMPLE)
         0x00, 0x00, // misc
         0x49, 0x00, // size
         0x10, 0x12, 0x33, 0x48, 0x99, 0x1A, 0x2B, 0x3C, // ip

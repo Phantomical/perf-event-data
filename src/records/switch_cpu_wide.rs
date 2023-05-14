8d8a295d7fb2df4f6152845ee83c603283c176ef
Sean Lynch <phantom@lynches.ca>
2023-05-14 13:43:38 -0700

    Modify SwitchCpuWide to have both in and out variants

--- src/records/switch_cpu_wide.rs
+++ src/records/switch_cpu_wide.rs
@@ -1,3 +1,5 @@
+use perf_event_open_sys::bindings::PERF_RECORD_MISC_SWITCH_OUT;
+
 use crate::prelude::*;
 
 /// SWITCH_CPU_WIDE records indicates a context switch when profiling in
@@ -15,18 +17,40 @@ use crate::prelude::*;
 ///
 /// [manpage]: http://man7.org/linux/man-pages/man2/perf_event_open.2.html
 #[derive(Copy, Clone, Debug)]
-pub struct SwitchCpuWide {
-    /// The process ID associated with the context switch.
-    ///
-    /// Depending on whether this is a switch-in or a switch-out this will be
-    /// the incoming process ID or outgoing process ID, respectively.
-    pub pid: u32,
+pub enum SwitchCpuWide {
+    /// A context switch into the current process.
+    In {
+        /// The process ID of the process being switched into.
+        pid: u32,
 
-    /// The thread ID associated with the context switch.
-    ///
-    /// Depending on whether this is a switch-in or a switch-out this will be
-    /// the incoming thread ID or the outgoing thread ID, respectively.
-    pub tid: u32,
+        /// The thread IF of the thread being switched into.
+        tid: u32,
+    },
+
+    /// A context switch away from the current process.
+    Out {
+        /// The process ID of the process being switched away from.
+        pid: u32,
+
+        /// The thread ID of the thread being switched away from.
+        tid: u32,
+    },
+}
+
+impl SwitchCpuWide {
+    /// The process ID associated with the switch.
+    pub fn pid(&self) -> u32 {
+        match *self {
+            Self::In { pid, .. } | Self::Out { pid, .. } => pid,
+        }
+    }
+
+    /// The thread ID associated with the switch.
+    pub fn tid(&self) -> u32 {
+        match *self {
+            Self::In { tid, .. } | Self::Out { tid, .. } => tid,
+        }
+    }
 }
 
 impl<'p> Parse<'p> for SwitchCpuWide {
@@ -35,9 +59,13 @@ impl<'p> Parse<'p> for SwitchCpuWide {
         E: Endian,
         B: ParseBuf<'p>,
     {
-        Ok(Self {
-            pid: p.parse()?,
-            tid: p.parse()?,
-        })
+        let pid = p.parse()?;
+        let tid = p.parse()?;
+
+        if p.config().misc() & PERF_RECORD_MISC_SWITCH_OUT as u16 != 0 {
+            Ok(Self::Out { pid, tid })
+        } else {
+            Ok(Self::In { pid, tid })
+        }
     }
 }
